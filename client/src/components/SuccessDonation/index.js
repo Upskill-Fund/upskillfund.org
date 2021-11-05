d2b7a76caca0527cb13fd792fc87f869d3141c29
charishma-sv <itscharishma@gmail.com>
2021-11-05 07:40:47 -0700

    donation page

--- client/src/components/SuccessDonation/index.js
+++ client/src/components/SuccessDonation/index.js
@@ -3,7 +3,7 @@ import { withRouter } from 'react-router-dom';
 function SuccessDonation({ history }) {
   return (
     <div class="container d-flex flex-column align-items-center">
-      <h1>Thank you for the generous donation</h1>
+      <h1>Thank you for the generous donation </h1>
       <button className="btn btn-primary" onClick={() => history.push('/')}>
         {' '}
         Go to Home
