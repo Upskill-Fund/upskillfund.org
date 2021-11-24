4b3edc6e6aadf0012fd4381c3598b94c727ff008
charishma-sv <itscharishma@gmail.com>
2021-11-24 00:01:44 -0800

    Updated Footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,42 +1,83 @@
 import React from 'react';
-import { Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import logo from '../../images/Upskill Logo/PNG/Logo White.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faFacebookSquare,
+  faInstagram,
+  faTwitterSquare,
+  faYoutubeSquare,
+} from '@fortawesome/free-brands-svg-icons';
+
 function Footer() {
   return (
-    <div className="footer">
+    <footer className="pb-4 pt-4 bg-dark text-white">
+      <div className="text-center mb-4">
+        <img src={logo} alt="upskill-logo" width="90" />
+      </div>
       <div className="container">
-        <div className="row text-center top-footer-nav">
-          <div className="col">
-            <Link to="/contact" className="btn">
-              Contact Us
-            </Link>
+        <div className="row justify-content-md-center text-center">
+          <div className="col-12 col-md-2 mb-5">
+            <h4>Upskill-Fund</h4>
+            <p>
+              <Link to="/">Home</Link>
+            </p>
           </div>
-          <div className="col">Media</div>
-          <div className="col-md-12 text-center bottom-footer-nav">
-            <Nav as="ul" className="justify-content-center">
-              <Nav.Item as="li">
-                <Nav.Link eventKey="link-1">Reports</Nav.Link>
-              </Nav.Item>
-              <Nav.Item as="li">
-                <Nav.Link eventKey="link-2">Financial Overview</Nav.Link>
-              </Nav.Item>
-              <Nav.Item as="li">
-                <Nav.Link eventKey="link-2">Anti-Corruption Policy</Nav.Link>
-              </Nav.Item>
-              <Nav.Item as="li">
-                <Nav.Link eventKey="link-2">Credits</Nav.Link>
-              </Nav.Item>
-              <Nav.Item as="li">
-                <Nav.Link eventKey="link-2">Privacy Policy</Nav.Link>
-              </Nav.Item>
-              <Nav.Item as="li">
-                <Nav.Link eventKey="link-2">Terms of Use</Nav.Link>
-              </Nav.Item>
-            </Nav>
+          <div className="col-12 col-md-2 mb-5">
+            <h4>About</h4>
+            <p>
+              <Link to="/about">Mission Statement</Link>
+            </p>
+          </div>
+          <div className="col-12 col-md-2 mb-5">
+            <h4>Contact</h4>
+            <p>
+              <Link to="/contact">Contact us</Link>
+            </p>
+          </div>
+          <div className="col-12 col-md-2 mb-5">
+            <h4>Donate</h4>
+            <p>
+              <Link to="/donate">Donate</Link>
+            </p>
+          </div>
+          <div className="col-12 col-md-2 mb-5">
+            <h4>Legal</h4>
+            <p>
+              <Link to="/">Privacy</Link>
+            </p>
           </div>
         </div>
       </div>
-    </div>
+      <hr></hr>
+      <div className="container">
+        <div className="row justify-content-center text-center mt-4 mb-4">
+          <div className="col-1">
+            <a href="#">
+              <FontAwesomeIcon icon={faFacebookSquare} size="2x" />
+            </a>
+          </div>
+          <div className="col-1">
+            <a href="#">
+              <FontAwesomeIcon icon={faTwitterSquare} size="2x" />
+            </a>
+          </div>
+          <div className="col-1">
+            <a href="#">
+              <FontAwesomeIcon icon={faInstagram} size="2x" />
+            </a>
+          </div>
+          <div className="col-1">
+            <a href="#">
+              <FontAwesomeIcon icon={faYoutubeSquare} size="2x" />
+            </a>
+          </div>
+        </div>
+        <div className="text-center">
+          <p>Copyright © Upskill Fund, LLC 2021</p>
+        </div>
+      </div>
+    </footer>
   );
 }
 
